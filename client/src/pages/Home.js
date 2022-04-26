212b84234deac8ddb851c6f0c6e3241b8a04e593
francis-mwas <mwangif184@gmail.com>
2022-04-26 13:56:50 +0300

    Added client app architecture

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hero from '../componnts/Hero';
+
+function Home() {
+  return (
+    <div className="home">
+      <Hero />
+    </div>
+  );
+}
+export default Home;
